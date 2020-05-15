dce8e4a0b6572e5e4c19f5fb853a30bbc0364241
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-05-15 14:48:10 -0300

    Refactores functionality for book creation

---
+++ src/main/java/com/jackelinenascimento/bookstoremanager/controller/BookController.java
@@ -0,0 +1,29 @@
+package com.jackelinenascimento.bookstoremanager.controller;
+
+import com.jackelinenascimento.bookstoremanager.dto.MessageResponseDTO;
+import com.jackelinenascimento.bookstoremanager.entity.Book;
+import com.jackelinenascimento.bookstoremanager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody Book book) {
+        return bookService.create(book);
+    }
+
+}
---
+++ src/main/java/com/jackelinenascimento/bookstoremanager/service/BookService.java
@@ -0,0 +1,28 @@
+package com.jackelinenascimento.bookstoremanager.service;
+
+import com.jackelinenascimento.bookstoremanager.dto.MessageResponseDTO;
+import com.jackelinenascimento.bookstoremanager.entity.Book;
+import com.jackelinenascimento.bookstoremanager.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public MessageResponseDTO create(Book book) {
+        Book savedBook = bookRepository.save(book);
+        return MessageResponseDTO.builder()
+                .message("Book created with ID " + savedBook.getId())
+                .build();
+
+    }
+
+}
